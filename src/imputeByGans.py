a5280aa6f3e77b4de4bbfd6dbaeb6ca26b067b0f
Yungang Xu <xuyungang84@gmail.com>
2020-07-17 01:29:31 -0400

    Update imputeByGans.py

--- src/imputeByGans.py
+++ src/imputeByGans.py
@@ -50,11 +50,10 @@ parser.add_argument('--threthold', type=float, default=0.01, help='the convergen
 parser.add_argument('--job_name', type=str, default="", help='the user-defined job name, which will be used to name the output files.')
 parser.add_argument('--outdir', type=str, default=".", help='the directory for output.')
 
-max_ncls = opt.ncls  # 
 
 opt = parser.parse_args()
-#opt.impute=True
-#print(opt)
+max_ncls = opt.ncls  # 
+
 job_name = opt.job_name
 GANs_models = opt.outdir+'/GANs_models'
 if (job_name == ""):
